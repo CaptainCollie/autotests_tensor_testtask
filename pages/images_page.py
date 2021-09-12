0b8f4972ad3a40adca255121f4b15f42913966d2
CaptainCollie <30548957+CaptainCollie@users.noreply.github.com>
2021-09-12 23:27:20 +0800

    Update images_page.py

--- pages/images_page.py
+++ pages/images_page.py
@@ -5,7 +5,7 @@ from .locators import ImagesPageLocators
 class ImagesPage(BasePage):
 
     def should_be_images_page(self):
-        assert 'image' in self.browser.current_url, 'It is not images page'
+        assert 'https://yandex.ru/images/' in self.browser.current_url, 'It is not images page'
 
     def click_on_first_category(self):
         text = self.browser.find_element(*ImagesPageLocators.FIRST_CATEGORY).text
